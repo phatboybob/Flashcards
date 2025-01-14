a0d05a21ebdcc6cdd8c8469ac7cb449c73da814d
phatboybob <lorianne.jack@gmail.com>
2025-01-14 14:36:16 -0500

    cleaned up data, added commends, and docstrings (#4)

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,10 @@
+'''
+Flashcards App Created (for now) to run English to German or German to English
+translations.
+
+Created by Lori Jackson January 2025
+'''
+
 import streamlit as st
 
 from utils import (
@@ -23,16 +30,15 @@ DIRECTION_GERMAN = 'German'
 OTHER_DIRECTION = ''
 
 
-st.set_page_config(page_title="Flashcards",
-                   layout="wide",
+st.set_page_config(page_title='Flashcards',
+                   layout='wide',
                    )
-
 
 
 st.session_state.flashcards_df = load_flashcard_data()
 
-if "flashcards_df" not in st.session_state:
-    flashcards_data = st.file_uploader("Choose your flashcards csv")
+if 'flashcards_df' not in st.session_state:
+    flashcards_data = st.file_uploader('Choose your flashcards csv')
     if flashcards_data is not None:
         load_flashcard_data(flashcards_data)
 
@@ -40,70 +46,88 @@ review_tab, view_tab = st.tabs(['Review', 'Modify/View'])
 with review_tab:
     with st.form('vocab_list_form'):
         parameters_container = st.container()
-        number_to_ask = parameters_container.text_input(label="Number of words to ask",
+        number_to_ask = parameters_container.text_input(label='Number of words to ask',
                                                         value=5,
                                                         help=('This is the number of words '
-                                                            'that will be asked in this session'))
+                                                              'that will be asked in this session'))
         correct_count = parameters_container.text_input(label='Only show if correct less than:',
                                                         value=10,
                                                         help=('I\'ll only show words that have been called '
-                                                            'less than this number of times AND are right less than '
-                                                            'the below percentage'
-                                                            '  \nExample: 10, 70% --> all words that have only been asked'
-                                                            '10 or fewer times and were correct 70% of the time'))
+                                                              'less than this number of times AND are right less than '
+                                                              'the below percentage'
+                                                              '  \nExample: 10, 70% --> all words that have only been asked'
+                                                              '10 or fewer times and were correct 70% of the time'))
         percent_correct = parameters_container.text_input(label='% right less than',
-                                                        value=100,
-                                                        help=('I\'ll only show words that have been called '
-                                                            'less than above number of times AND are right less than '
-                                                            'this percentage'
-                                                            '  \nExample: 10, 70% --> all words that have only been asked'
-                                                            '10 or fewer times and were correct 70% of the time'))
-        # direction = parameters_container.selectbox('Translate Diretion',
-        #                                             [DIRECTION_GERMAN,
-        #                                             DIRECTION_ENGLISH])
-        # other_direction = set_other_direction(direction=direction)
+                                                          value=100,
+                                                          help=('I\'ll only show words that have been called '
+                                                                'less than above number of times AND are right less than '
+                                                                'this percentage'
+                                                                '  \nExample: 10, 70% --> all words that have only been asked'
+                                                                '10 or fewer times and were correct 70% of the time'))
 
         show_selection = st.form_submit_button('Set Parameters')
+
     if show_selection:
         params = set_params(number_to_ask=number_to_ask,
                             correct_count=correct_count,
                             percent_correct=percent_correct,
                             )
 
-
     german_to_english_tab, english_to_german_tab = st.tabs(['Translate German to English',
                                                             'Translate English to German'])
     with german_to_english_tab:
         with st.form('German to English', clear_on_submit=True):
-            st.session_state.my_answer = st.text_input(label="Type Answer Here:")
+            st.session_state.my_answer = st.text_input(label='Type Answer Here:')
             submit = st.form_submit_button('Run German to English')
             if submit:
+                # get a subset of words based on the parameters
                 if 'sample' not in st.session_state:
                     st.session_state.sample = get_vocab_sample(number_to_ask=number_to_ask,
                                                                percent_correct=percent_correct,
                                                                correct_count=correct_count,
                                                                direction = DIRECTION_GERMAN
                                                               )
+                    # code removes word from sample as user gets it right.
+                    # code updates sample_copy with correct counts, then merges
+                    # that into the original dataframe
                     st.session_state.sample_copy = st.session_state.sample
+
+                # set word to ask and display
                 if 'word' not in st.session_state:
                     if len(st.session_state.sample)>0:
                         # get the row from the sample
-                        # set the "word" and the "answer"
-                        set_word_line_values(direction=DIRECTION_GERMAN, other_direction=set_other_direction(DIRECTION_GERMAN))
+                        # set the 'word' and the 'answer'
+                        set_word_line_values(direction=DIRECTION_GERMAN,
+                                             other_direction=set_other_direction(DIRECTION_GERMAN))
                         st.markdown(f'# {st.session_state.word}')
                     else:
                         st.markdown('Parameters are too strict, no words in sample size')
+
+                # if 'word' is in session state, then check if the answer is correct
                 else:
                     if check_word(DIRECTION_GERMAN) is True:
-                        update_correct_word(direction=DIRECTION_GERMAN, from_word=st.session_state.word, df=st.session_state.sample_copy)
+                        update_correct_word(direction=DIRECTION_GERMAN,
+                                            from_word=st.session_state.word,
+                                            df=st.session_state.sample_copy)
                         remove_word(direction=DIRECTION_GERMAN)
+
+                        # if user hasn't gotten them all correct, set a new word
+                        # from the sample
                         if len(st.session_state.sample) > 0:
-                            set_word_line_values(direction=DIRECTION_GERMAN, other_direction=set_other_direction(DIRECTION_GERMAN))
+                            set_word_line_values(direction=DIRECTION_GERMAN,
+                                                 other_direction=set_other_direction(DIRECTION_GERMAN))
                             st.markdown(f'# {st.session_state.word}')
                         else:
-                            st.markdown('# You got them all correct. Hit "Show Selection" to get a new selection of words')
-                            merge_and_print_dataframes(st.session_state.flashcards_df, st.session_state.sample_copy)
-                            st.markdown(f'# Summary: \n {view_flashcard_table(st.session_state.sample_copy)}')
+                            st.markdown('# You got them all correct. '
+                                        'Hit "Show Selection" to get a new selection of words')
+
+                            # merge the updated correct counts with
+                            # original data and print to file
+                            merge_and_print_dataframes(st.session_state.flashcards_df,
+                                                       st.session_state.sample_copy)
+                            st.markdown('# Summary: \n '
+                                        f'{view_flashcard_table(
+                                            st.session_state.sample_copy)}')
 
                             clear_values()
                     else:
@@ -120,24 +144,41 @@ with review_tab:
     with english_to_german_tab:
         with st.form('English to German'):
             submit_english_to_german = st.form_submit_button('Run English to German/Show Answer')
+
+            # make sure yes/no buttons disabled until ready
             if 'yes_no_disabled' not in st.session_state:
                 st.session_state.yes_no_disabled = True
+
             yes_col, no_col = st.columns(2)
             with yes_col:
-                st.session_state.yes_button = st.form_submit_button(label='Yes', disabled=st.session_state.yes_no_disabled)
+                st.session_state.yes_button = st.form_submit_button(label='Yes',
+                                                                    disabled=st.session_state.yes_no_disabled)
             with no_col:
-                # For some reason on_click can only be in one (and only the second) one of these buttons. Otherwise "no" won't work
-                st.session_state.no_button = st.form_submit_button(label='No', disabled=st.session_state.yes_no_disabled, on_click=disable_buttons())
+                # I think there's a bug in streamlit (14 jan 2025), can only have
+                # one 'on click' and it has to be the second button.
+                st.session_state.no_button = st.form_submit_button(label='No',
+                                                                   disabled=st.session_state.yes_no_disabled,
+                                                                   on_click=disable_buttons())
 
             if submit_english_to_german:
+
+                # get a subset of words based on the parameters
                 if 'sample' not in st.session_state:
                     st.session_state.sample = get_vocab_sample(number_to_ask=number_to_ask,
                                                                percent_correct=percent_correct,
                                                                correct_count=correct_count,
                                                                direction = DIRECTION_ENGLISH
                                                               )
+
+                    # code removes word from sample as user gets it right.
+                    # code updates sample_copy with correct counts, then merges
+                    # that into the original dataframe
                     st.session_state.sample_copy = st.session_state.sample
+
+                    # this happens on next page refresh (like pressing a button)
                     enable_buttons()
+
+                # set word to ask and display
                 if 'word' in st.session_state:
                     st.markdown(f'# :blue[{st.session_state.word}] is '
                                 f':green[{st.session_state.correct_answer}] in Geramn'
@@ -145,34 +186,43 @@ with review_tab:
                 else:
                     if len(st.session_state.sample)>0:
                         # get the row from the sample
-                        # set the "word" and the "answer"
-                        set_word_line_values(direction=DIRECTION_ENGLISH, other_direction=set_other_direction(DIRECTION_ENGLISH))
+                        # set the 'word' and the 'answer'
+                        set_word_line_values(direction=DIRECTION_ENGLISH,
+                                             other_direction=set_other_direction(DIRECTION_ENGLISH))
                         st.markdown(f'# {st.session_state.word}')
                     else:
                         st.markdown('Parameters are too strict, no words in sample size')
+
             if st.session_state.yes_button:
                 # This executes for the next button pressed,
-                # which should only ever be "Run English to German/Show Answer"
+                # which should only ever be 'Run English to German/Show Answer'
                 enable_buttons()
-                update_correct_word(direction=DIRECTION_ENGLISH, from_word=st.session_state.word, df=st.session_state.sample_copy)
+                update_correct_word(direction=DIRECTION_ENGLISH,
+                                    from_word=st.session_state.word,
+                                    df=st.session_state.sample_copy)
                 remove_word(direction=DIRECTION_ENGLISH)
                 if len(st.session_state.sample) > 0:
-                    set_word_line_values(direction=DIRECTION_ENGLISH, other_direction=set_other_direction(DIRECTION_ENGLISH))
+                    set_word_line_values(direction=DIRECTION_ENGLISH,
+                                         other_direction=set_other_direction(DIRECTION_ENGLISH))
                     st.markdown(f'# {st.session_state.word}')
                 else:
-                    st.markdown('# You got them all correct. Hit "Show Selection" to get a new selection of words')
-                    merge_and_print_dataframes(st.session_state.flashcards_df, st.session_state.sample_copy)
-                    st.markdown(f'# Summary: \n {view_flashcard_table(st.session_state.sample_copy)}')
+                    st.markdown('# You got them all correct. '
+                                'Hit "Show Selection" to get a new selection of words')
+                    merge_and_print_dataframes(st.session_state.flashcards_df,
+                                               st.session_state.sample_copy)
+                    st.markdown('# Summary: \n '
+                                f'{view_flashcard_table(st.session_state.sample_copy)}')
                     del st.session_state.yes_no_disabled
                     clear_values()
             if st.session_state.no_button:
                 # This executes for the next button pressed,
-                # which should only ever be "Run English to German/Show Answer"
+                # which should only ever be 'Run English to German/Show Answer'
                 enable_buttons()
                 update_incorrect_word(direction=DIRECTION_ENGLISH,
-                                        from_word=st.session_state.word,
-                                        df=st.session_state.sample_copy)
-                set_word_line_values(direction=DIRECTION_ENGLISH, other_direction=set_other_direction(DIRECTION_ENGLISH))
+                                      from_word=st.session_state.word,
+                                      df=st.session_state.sample_copy)
+                set_word_line_values(direction=DIRECTION_ENGLISH,
+                                     other_direction=set_other_direction(DIRECTION_ENGLISH))
                 st.markdown(f'# {st.session_state.word}')
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+"""
+Utilities for the Streamlit German Flashcards app
+Created by Lori Jackson January 2025
+"""
+
 import os
 
 import pandas
@@ -12,10 +17,9 @@ COLUMNS_AND_TYPES = {f'{DIRECTION_ENGLISH} Word': str,
                      f'{DIRECTION_ENGLISH} Call Count': 'Int64',
                      f'{DIRECTION_ENGLISH} Percent Correct': float,
                      f'{DIRECTION_GERMAN} Word': str,
-                    f'{DIRECTION_GERMAN} Correct Count': 'Int64',
-                    f'{DIRECTION_GERMAN} Call Count': 'Int64',
-                    f'{DIRECTION_GERMAN} Percent Correct': float}
-
+                     f'{DIRECTION_GERMAN} Correct Count': 'Int64',
+                     f'{DIRECTION_GERMAN} Call Count': 'Int64',
+                     f'{DIRECTION_GERMAN} Percent Correct': float}
 
 
 def load_flashcard_data(flashcard_path=LORIS_FLASHCARDS_CSV):
@@ -31,6 +35,7 @@ def load_flashcard_data(flashcard_path=LORIS_FLASHCARDS_CSV):
             dtype=COLUMNS_AND_TYPES,
         ).fillna(0)
         return flashcards_df
+
 
 def view_flashcard_data_editor(flashcards_df):
     """_summary_
@@ -55,6 +60,7 @@ def view_flashcard_table(flashcards_df):
     else:
         st.write("please upload data")
 
+
 def get_vocab_sample(number_to_ask,
                      percent_correct,
                      correct_count,
@@ -62,24 +68,29 @@ def get_vocab_sample(number_to_ask,
                      ):
     """get a sample from the huge list of words
     Args:
-        number_to_ask (int): _description_
-        percent_corret (float): _description_
-        correct_count (int): _description_
+        number_to_ask (int): how man words to ask (maximum)
+        percent_corret (float): only show words that you've gotten right less than this
+        correct_count (int): only show words that have been correct less than this
     """
     # entire dataframe
     all_words = st.session_state.flashcards_df
 
     # all words that fit the ask count and percent cuttoff
-    fewer_words = all_words[(all_words[f'{direction} Correct Count'] <= int(correct_count)) & (all_words[f'{direction} Percent Correct'] <= float(percent_correct))]
+    fewer_words = all_words[(all_words[f'{direction} Correct Count'] <= int(correct_count))
+                            & (all_words[f'{direction} Percent Correct'] <= float(percent_correct))]
 
     # subset of qualifying words
+    # subset may be less than the number to ask, leading to out of bounds error
     if len(fewer_words) <= int(number_to_ask):
         return fewer_words
     else:
         return fewer_words.sample(n=int(number_to_ask))
 
+
 def check_word(direction):
     """Check if the value entered was correct
+    Args:
+        direction: asking a German word to English, or the inverse.
     """
     if direction == 'German':
         if st.session_state.my_answer.lower() == st.session_state.correct_answer.lower():
@@ -89,6 +100,14 @@ def check_word(direction):
 
 
 def set_other_direction(direction):
+    """Sets the inverse direction (English to German or German to English)
+
+    Args:
+        direction (String): Asking a German -> English or Enlgish -> German
+
+    Returns:
+        string: Returns the opposite of direction
+    """
     if direction is DIRECTION_ENGLISH:
         return DIRECTION_GERMAN
     else:
@@ -96,17 +115,15 @@ def set_other_direction(direction):
 
 
 def set_params(number_to_ask,
-                   correct_count,
-                   percent_correct,
-                   ):
-    """_summary_
+               correct_count,
+               percent_correct,
+              ):
+    """Sets the parameters for what words to ask
 
     Args:
-        number_to_ask (_type_): _description_
-        correct_count (_type_): _description_
-        percent_correct (_type_): _description_
-        direction (_type_): _description_
-        other_direction (_type_): _description_
+        number_to_ask (int): number of words to ask
+        correct_count (int): number of time correct threashold
+        percent_correct (float): percent correct threashold
     """
     clear_values()
     return {'number_to_ask': number_to_ask,
@@ -116,64 +133,101 @@ def set_params(number_to_ask,
 
 
 def set_word_line_values(direction, other_direction):
-    """_summary_
-
+    """Once a subset of words are selected based on the parameters,
+    This selects a word from the sample/subset and assigns the entire
+    row of data from the dataframe Example:
+    he must	11	37	0.2972972972972973	er muss	4	6	0.6666666666666666
     Args:
-        direction (_type_): _description_
-        other_direction (_type_): _description_
+        direction (string): English -> German or German -> English
+        other_direction (string): opposite of above
     """
+    # take a sample of the samle date (get a random row)
     st.session_state.word_line = st.session_state.sample.sample()
+
+    # set the word to be asked
     st.session_state.word = st.session_state.word_line[f'{direction} Word'].values[0]
-    st.session_state.correct_answer = st.session_state.word_line[f'{other_direction} Word'].values[0]
+
+    # set the correct answer
+    st.session_state.correct_answer = st.session_state.word_line[
+        f'{other_direction} Word'].values[0]
 
 def update_correct_word(direction, from_word, df):
-    """updates when a word is correct"""
+    """updates when a word is correct
+    Args:
+        direction (string): Same as all the others of this
+        from_word (string): word that user tried to translate
+        df (Datframe): Dataframe containing the sample flashcard data.
+            ie, what needs to be updated.
+    """
+
+    # get the current number of times user has answered correctly
     correct_count = df.loc[df[f'{direction} Word'] == from_word, f'{direction} Correct Count']
+
+    # get the current number of times the user has been asked this word
     call_count = df.loc[df[f'{direction} Word'] == from_word, f'{direction} Call Count']
 
     correct_count += 1
     call_count += 1
     correct_percent = correct_count/call_count
 
+    # update sample dataframe with new counts and percent correct
     df.loc[df[f'{direction} Word'] == from_word, f'{direction} Call Count'] = call_count
     df.loc[df[f'{direction} Word'] == from_word, f'{direction} Correct Count'] = correct_count
     df.loc[df[f'{direction} Word'] == from_word, f'{direction} Percent Correct'] = correct_percent
 
 
 def update_incorrect_word(direction, from_word, df):
-    """updates when a word is incorrect"""
+    """updates when a word is incorrect
+    Args:
+        direction (string): Same as all the others of this
+        from_word (string): word that user tried to translate
+        df (Datframe): Dataframe containing the sample flashcard data.
+            ie, what needs to be updated.
+    """
+
+    # get the current number of times user has answered correctly
     correct_count = df.loc[df[f'{direction} Word'] == from_word, f'{direction} Correct Count']
+
+    # get the current number of times the user has been asked this word
     call_count = df.loc[df[f'{direction} Word'] == from_word, f'{direction} Call Count']
-    print(f'{from_word}: Before correct count: {correct_count.to_string(index=False)}, call count: {call_count.to_string(index=False)}')
 
     call_count += 1
     correct_percent = correct_count/call_count
 
+    # update sample dataframe with new counts and percent correct
     df.loc[df[f'{direction} Word'] == from_word, f'{direction} Call Count'] = call_count
     df.loc[df[f'{direction} Word'] == from_word, f'{direction} Percent Correct'] = correct_percent
-    print(f'{from_word} After: correct count: {correct_count.to_string(index=False)}, call count: {call_count.to_string(index=False)}, correct_percent: {correct_percent.to_string(index=False)}')
 
 
 def merge_and_print_dataframes(old_df, new_df):
     """Merges the new and old dataframe, overwriting the old with the new
 
     Args:
-        new_df (_type_): _description_
-        old_df (_type_): _description_
+        new_df (Dataframe): The sample dataframe that was temporarily holding the
+                            updated values of correct/incorrect
+        old_df (Dateframe): The main dataframe of vocab words
+                            that new_df will be merged into
     """
+    # merge the sample dataframe into the main dataframe
     old_df.update(new_df)
+
+    # write that shit out
     old_df.to_csv(LORIS_FLASHCARDS_CSV, index=False, header=False)
 
 def remove_word(direction):
-    """remove words from the sample
+    """remove words from the sample. Once all the words are gone,
+    user is done with the session
 
     Args:
-        direction (_type_): _description_
+        direction (string): uhg. Again? I need to make this a session state
     """
-    st.session_state.sample = st.session_state.sample.drop(st.session_state.sample[st.session_state.sample[f'{direction} Word'] == st.session_state.word].index)
+    st.session_state.sample = st.session_state.sample.drop(
+        st.session_state.sample[
+            st.session_state.sample[f'{direction} Word'] == st.session_state.word].index)
 
 def clear_values():
-    """clear the values so starting fresh doesn't have session states messing up if statements
+    """clear the values so starting fresh doesn't
+    have session states messing up if statements
     """
     values = ['word_line', 'word', 'correct_answer', 'sample']
 
@@ -181,49 +235,27 @@ def clear_values():
         if val in st.session_state:
             del st.session_state[val]
 
-def run_english_to_german():
-    """_summary_
-    """
-    if 'word' not in st.session_state:
-            # get the row from the sample
-            # set the "word" and the "answer"
-        if len(st.session_state.sample) > 0:
-            set_word_line_values(direction='English', other_direction='German')
-            st.markdown(f'# {st.session_state.word}')
-            st.markdown(f'the sample data is: {st.session_state.sample}')
-
-        else:
-            st.markdown('# You got them all correct. Hit '
-                        '"Show Selection" to get a new selection of words'
-                        f'the sample data is: {st.session_state.sample}')
-            print(f'the selection is {st.session_state.sample}')
-
-            clear_values()
-    else:
-        st.markdown('# The correct '
-                    f' answer for :blue[{st.session_state.word}] is '
-                    f':green[{st.session_state.correct_answer}] '
-                    'Did you get it right?')
 
 def enable_yes_no():
-    """enable the button. This is backwards and I don't know why
+    """Enable any buttons with the session state of 'disabled' (yes and no)
     """
-    if "disabled" in st.session_state and st.session_state.disabled == False:
+    if 'disabled' in st.session_state and st.session_state.disabled is False:
         st.session_state.disabled = True
 
 def disable_yes_no():
-    """Set the enable button to "False". This works backwards and I don't know why
+    """disable any buttons with the session state of 'disabled' (yes and no)
     """
-    if "disabled" in st.session_state and st.session_state.disabled == True:
+    if 'disabled' in st.session_state and st.session_state.disabled is True:
         st.session_state.disabled = False
 
 
 def enable_buttons():
-    """Enables the yes_no buttons"""
+    """Enable any buttons with the session state of 'disabled' (yes and no)
+    """
     print('method call set it to enabled')
     st.session_state.yes_no_disabled = False
 
 def disable_buttons():
-    """Disabled the yes_no buttons"""
+    """Disable any buttons with the session state of 'disabled' (yes and no)
+    """
     st.session_state.yes_no_disabled = True
-
